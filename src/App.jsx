69eedac03593a8390059ff0c1efde618d058c172
TABISH <tabishjaved@hotmail.com>
2024-11-11 22:48:19 +0530

    building the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
+import Homepage from "./pages/Homepage"
 import AppLayout from "./pages/AppLayout"
 import Product from "./pages/Product"
 import Pricing from "./pages/Pricing"
 import PageNotFound from "./pages/PageNotFound"
+import Login from "./pages/Login"
 
 
 function App () {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={ "/" } element={ <Home /> } />
-        <Route path={ "product" } element={ <Product /> } />
-        <Route path={ "pricing" } element={ <Pricing /> } />
+        <Route path={ "/" } element={ <Homepage /> } />
+        <Route path={ "/product" } element={ <Product /> } />
+        <Route path={ "/pricing" } element={ <Pricing /> } />
+        <Route path={ "/login" } element={ <Login /> } />
         <Route path="app" element={ <AppLayout /> } />
         <Route path={ "*" } element={ <PageNotFound /> } />
       </Routes>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from "../components/PageNav"
-
-function PageNotFound () {
+export default function PageNotFound() {
   return (
-    <>
-      <PageNav />
-      <div>Page Not Found!</div>
-    </>
-  )
+    <div>
+      <h1>Page not found 😢</h1>
+    </div>
+  );
 }
-
-export default PageNotFound
